64013ac6a04a05bca87455726e7c2f5e8b755ff5
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2024-12-05 13:54:18 +0100

    update

--- setup.R
+++ setup.R
@@ -1,7 +1,11 @@
 if(!"pak" %in% installed.packages()){install.packages("pak")}
 pak::pkg_install("callr")
 
-stopifnot(grep("/nd.site$", getwd()))
+getwd() |>
+  fs::path_split() |>
+  purrr::pluck(1, -1) |>
+  identical("nd.site") |>
+  stopifnot()
 
 callr::r(\(){
   renv::init()
@@ -11,7 +15,7 @@ callr::r(\(.conda_envname){
   .python_path <- reticulate::conda_create(envname=.conda_envname, python_version="3.10")
   reticulate::conda_install(envname=.conda_envname, packages="conda-forge::hugo")
   renv::use_python(.python_path, type="conda", name=.conda_envname)
-}, args=list(.conda_envname="nd.util.condaenv"))
+}, args=list(.conda_envname="nd.site.condaenv"))
 
 callr::r(\(.conda_envname){
   renv::snapshot(prompt=FALSE)
@@ -19,3 +23,5 @@ callr::r(\(.conda_envname){
     purrr::discard_at("prefix") |>
     yaml::write_yaml("environment.yml")
 })
+
+
